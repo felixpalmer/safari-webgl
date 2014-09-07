6e1af35e62df53b25fa8d9fb85ab3c1f4401d610
Felix Palmer <felixpalmer@gmail.com>
2014-09-07 17:33:34 +0200

    Reattach renderer when slide changes

--- js/presentation.js
+++ js/presentation.js
@@ -1,6 +1,6 @@
 /*global shower*/
 // Start the app
-require( ['detector', 'app', 'container'], function ( Detector, app, container ) {
+require( ['detector', 'app', 'container', 'renderer'], function ( Detector, app, container, renderer ) {
   if ( ! Detector.webgl ) {
     Detector.addGetWebGLMessage();
     container.innerHTML = "";
@@ -8,11 +8,38 @@ require( ['detector', 'app', 'container'], function ( Detector, app, container )
 
   app.init();
   app.draw();
+  var lastSlideNumber = -1;
+  var rendering = false;
   var tick = function() {
     window.requestAnimationFrame( tick );
-    if ( shower.isSlideMode() && shower.getCurrentSlideNumber() === 0 ) {
-      app.spin = true;
-      app.draw();
+    if ( shower.isSlideMode() ) {
+      var slideNumber = shower.getCurrentSlideNumber();
+      if ( slideNumber !== lastSlideNumber ) {
+        // Have changed slide
+        lastSlideNumber = slideNumber;
+
+        // Check for presence of threejs container in new slide
+        var slideId = shower.getSlideHash( slideNumber ).slice( 1 );
+        var c = document.querySelector( "[id='" + slideId + "'] #threejs-container" );
+        if ( c ) {
+          rendering = true;
+          renderer.setContainer( c );
+        } else {
+          rendering = false;
+        }
+
+        // Set parameters for specific slide
+        if ( slideNumber === 0 ) {
+          app.spin = true;
+        }
+        if ( slideNumber === 1 ) {
+          app.spin = false;
+        }
+      }
+
+      if ( rendering ) {
+        app.draw();
+      }
     }
   };
   tick();
